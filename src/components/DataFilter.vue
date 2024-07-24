0a5fe3bc0403d3091d5bc79fffe562106df3163c
mikaeld <mducharme@mozilla.com>
2024-07-24 14:46:07 -0400

    Add simple search function and table display

--- src/components/DataFilter.vue
+++ src/components/DataFilter.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
+import { ref, computed } from 'vue'
 import DAWGTable from './DAWGTable.vue'
 
 const props = defineProps<{
@@ -17,20 +16,39 @@ const headers = ref([
     "Mouse Aware?"
 ])
 
-
 const projection = ref([
     ["Mikael", "Nano", "Smoked Things", "Yes"],
     ["Wesley", "Emacs", "Cost effective Brisket", "Unlikely"],
     ["Corban", "VSCode", "Pizza", "Somewhat"],
 ])
 
+const filteredProjection = computed(() => {
+    if (!searchstring.value) {
+        return projection.value
+    }
+    return projection.value.filter(row => 
+        row.some(cell => cell.toLowerCase().includes(searchstring.value.toLowerCase()))
+    )
+})
 </script>
 
 <template>
     <input type="text" v-model="searchstring" placeholder="Search"/>
-    <DAWGTable :headers="headers" :data="projection"/>
+    <DAWGTable :headers="headers" :rows="filteredProjection"/>
 </template>
 
 <style>
-/* Make it look like we care about it here  */
+input[type="text"] {
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #444;
+    border-radius: 4px;
+    background-color: #333;
+    color: #f1f1f1;
+    font-family: Arial, sans-serif;
+}
+
+input[type="text"]::placeholder {
+    color: #bbb;
+}
 </style>
