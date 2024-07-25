ca166a9fced2ecc10be7e8ba215e81a80fe428d4
Corban Cloud <ccloud@mozilla.com>
2024-07-25 10:22:45 -0700

    Add regex search

--- src/components/DataFilter.vue
+++ src/components/DataFilter.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { ref, computed, Computed } from 'vue'
+import { ref, Ref, computed, Computed } from 'vue'
 import { WorkGroup, fromDataSource } from '../workgroup.ts'
 import DAWGTable from './DAWGTable.vue'
 
+type CellContent = string | Map | Set;
+type SearchFunc = (contents: CellContent) => boolean;
 
 const props = defineProps<{
     data: any, 
@@ -19,52 +21,112 @@ const normalized: Computed<WorkGroup[]> = computed(() => {
     return result
 })
 
-// unused temporarily
-const searchstring = defineModel<string>()
+const isregex = defineModel<boolean>('isregex')
 
-// These will be generated from real data in the end
-const headers = ref((() => {
-    // get a null-ish workgroup and dump the keys
-    return Object.keys(fromDataSource("none", "placeholder", {}))
-})())
+const searchstring = defineModel<string>('searchstring')
+const searchregexp  = computed<RegExp>((): RegExp => {
+    if (!isregex) return undefined
+
+    return new RegExp(searchstring.value)
+})
+
+
+// Match plain strings
+const regularFilter: SearchFunc = (contents: string ): boolean => {
+    if (contents.toLowerCase == undefined) {
+        console.log(contents)
+        return false
+    }
+    return contents.toLowerCase().includes(searchstring.value.toLowerCase())
+}
+
+// match against regex
+// regex is pre-compiled via computed value so ignore first param
+const regexFilter: SearchFunc = (contents: string): boolean => {
+    return searchregexp.value.test(contents)
+}
+
+// Returns the correct filter func based on the toggle state
+const filterFunc: Computed<SearchFunc> = computed(() => {
+    return isregex.value ? regexFilter : regularFilter;
+})
 
 const filtered = computed(() => {
     if (!searchstring.value) {
         return normalized.value
     }
-    return normalized.value.filter(row => {
-       
-        return Object.values(row).some(cell => {
-            let contents: string = cell
-
-            if (cell instanceof Set || cell instanceof Map ) {
-                contents = Array.from(cell).join(" ")
+    return normalized.value.filter(row => {       
+        return Object.values(row).some(contents => {
+            if (contents instanceof Set) {
+                console.log("found a set")
+                return Array.from(contents).some((item) => filterFunc.value(item))
             }
-
-            return contents.toLowerCase().includes(searchstring.value.toLowerCase())
+            return filterFunc.value(contents)
         })
     })
 })
+
+const headers = ref((() => {
+    // get a null-ish workgroup and dump the keys
+    return Object.keys(fromDataSource("none", "placeholder", {}))
+})())
+
 </script>
 
 <template>
-    <input type="text" v-model="searchstring" placeholder="Search All Fields"/>
+    <div class="search-wrapper">
+        <input type="text" v-model="searchstring" placeholder="Search All Fields"/>
+
+        <div class="regex-toggle">
+            <input v-model="isregex"
+                type="checkbox"
+                name="regex"
+                value="regex"
+            />
+            <label for="regex"> .*</label>
+        </div>
+    </div>
     <DAWGTable :headers="headers" :rows="filtered"/>
 </template>
 
-<style>
-input {
+<style scoped>
+input[type=text] {
     padding: 10px;
     margin-bottom: 20px;
     border: 1px solid #444;
-    border-radius: 4px;
+    border-radius:  var(--border-radius) 0 0 var(--border-radius);
     background-color: #333;
     color: #f1f1f1;
     font-family: Arial, sans-serif;
 }
 
-input::placeholder {
+input[type=text]::placeholder {
     color: #e7efe8;
     opacity: 0.9;
+}
+
+.search-wrapper {
+    --border-radius: 4px;
+    width: 600px;
+    vertical-align: top;
+    padding: 0;
+}
+.search-wrapper [type=text] {
+    width: calc(100% - 60px);
+    display: inline-block;
+    height: 40px;
+}
+.regex-toggle {
+    display: inline-block;
+    border-radius: 0 var(--border-radius) var(--border-radius) 0;
+    width: 60px;
+    height: 40px;
+    background-color: #a5a5a5;
+    float: right;
+    text-align: center;
+    padding: 5px 0 0 0 ;
+}
+.regex-toggle label {
+    font-family: monospace;
 }
 </style>
