d47e72f8ad0d28a2d9224576f1f50897d1d640e3
mikaeld <mducharme@mozilla.com>
2024-07-25 15:24:45 -0400

    style dat regex

--- src/components/DataFilter.vue
+++ src/components/DataFilter.vue
@@ -84,82 +84,30 @@ const headers = ref((() => {
 <template>
     <div class="search-wrapper">
         <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-        <div class="relative mb-4">
+        <div class="relative mb-2">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
             </div>
             <input :class="{ monospace: isRegex }" type="search" v-model="searchstring" id="search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
-            <div class="regex-toggle absolute inset-y-0 start-100 flex items-center ps-3">
-                <input v-model="isRegex"
-                    type="checkbox"
-                    name="regex"
-                    value="regex"
-                />
-                <label for="regex"> .*</label>
-            </div>
         </div>
-        
-        <span class="regex-err" v-if="isRegex && searchregexp instanceof Error == true">⚠️ {{ searchregexp }}</span>
+        <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert" v-if="isRegex && searchregexp instanceof Error == true">
+          <span class="font-medium" v-if="isRegex && searchregexp instanceof Error == true">⚠️ {{ searchregexp }}</span>
+        </div>
     </div>
+    <div class="relative mb-2">
+      <label class="inline-flex items-center cursor-pointer">
+        <input v-model="isRegex" type="checkbox" name="regex" value="regex" class="sr-only peer">
+        <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Use Regex</span>
+      </label>
+    </div>
+
     <DAWGTable :headers="headers" :rows="filtered"/>
 </template>
 
 <style scoped>
-input[type=text] {
-    padding: 10px;
-    margin-bottom: 0px;
-    border: 1px solid #444;
-    border-radius:  var(--border-radius) 0 0 var(--border-radius);
-    background-color: #333;
-    color: #f1f1f1;
-    font-family: Arial, sans-serif;
 
-}
-input[type=text].monospace {
-    font-family: monospace;
-}
 
-input[type=text]::placeholder {
-    color: #e7efe8;
-    opacity: 0.9;
-}
-
-.search-wrapper {
-    --border-radius: 4px;
-    width: 600px;
-    vertical-align: top;
-    padding: 0;
-    margin-bottom: 0px;
-    width: 100%;
-}
-.search-wrapper #search {
-    width: calc(100% - 60px);
-    display: inline-block;
-    height: 40px;
-    margin: 10px 0;
-}
-.regex-toggle {
-    display: inline-block;
-    border-radius: 0 var(--border-radius) var(--border-radius) 0;
-    width: 60px;
-    height: 40px;
-    background-color: #a5a5a5;
-    float: right;
-    text-align: center;
-    padding: 5px 0 0 0 ;
-}
-.regex-toggle label {
-    font-family: monospace;
-}
-.regex-err {
-    display: block;
-    width: 100%;
-    background-color: #ffe674;
-    color: #8e8a17;
-    text-align: center;
-    padding: 12px;
-
-}
 </style>
