e91581f0a62068cb02a137a8ab5f8196df2a496d
mikaeld <mducharme@mozilla.com>
2024-07-26 09:21:31 -0400

    add navbar links, loading spinner and dark mode support for link logos

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,6 @@ onMounted(() => {
 <template>
   <header class="text-center">
     <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Effortlessly search and explore data access workgroups - Compiled from aggregated Terraform State data.</p>
-    <nav class="whd-links items-center">
-      <a href="https://github.com/mozilla/dawg/blob/whd/FAQ.md">Guide</a>
-      <a href="https://mozilla-hub.atlassian.net/wiki/spaces/SRE/pages/27924789/Data+Access+Workgroups">Data Access Workgroups Documentation</a>
-      <a href="https://mozilla-hub.atlassian.net/wiki/spaces/SRE/pages/27919274/Using+workgroups">GCPv2 Documentation</a>
-      <a href="https://wiki.mozilla.org/Security/Data_Classification">Data Classification at Mozilla</a>
-      <a href="https://wiki.mozilla.org/Data_Collection#Data_Collection_Categories">Data Collection at Mozilla</a>
-      <a href="https://groups.google.com/my-groups">Search for "da-wg" here to see your Data Access Workgroups</a>
-    </nav>
   </header>
   <main>
     <PolyAssetLoader :sources="['gcpv1_enriched.json','gcpv2_merged.json']" />
