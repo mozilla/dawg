3a63ade1967f71d732bf6ba25bf8d37c22e6b788
Corban Cloud <ccloud@mozilla.com>
2024-08-30 11:28:22 -0700

    Force test to fail to check report upload

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -32,3 +32,8 @@ test('check that we can toggle dark mode', async ({ page }) => {
   await page.locator('#theme-toggle').click()
   await expect(page.locator('html')).not.toHaveClass('dark')
 })
+
+test('forcefailure', async ({ page }) => {
+  await page.goto('/thisshouldfail')
+  await expect(false).toBeTruthy()
+})
