c4474a44c4b3cc231b59736b0446f68d61cf114a
Corban Cloud <ccloud@mozilla.com>
2024-09-18 13:58:54 -0700

    Move config to config file

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: { globals: true, environment: 'happy-dom' },
+  test: { globals: true, environment: 'happy-dom', exclude: ['node_modules/*', 'e2e/*'] },
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
