799a7029ff0e17975dc6557689acb412b482b51f
Corban Cloud <ccloud@mozilla.com>
2024-09-25 18:44:42 -0700

    use relative paths in urls (playwright convention), add meta test

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect } from '@playwright/test'
 
+// Tests the configuration of the vite+playwright system itself, if this fails ignore other test failures
+test('meta', async ({ page }) => {
+  const ideal = !!process.env.CI ? 'http://localhost:5173/dawg/' : 'http://localhost:5173/'
+  await page.goto('./')
+  await expect(page.url()).toBe(ideal)
+})
+
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
-  await page.goto('/')
+  await page.goto('./')
   await expect(page.locator('#home-link')).toBeInViewport()
 })
 
@@ -15,7 +22,7 @@ test('search for a DAWG', async ({ page }) => {
 })
 
 test('check that we can navigate to a detail page', async ({ page }) => {
-  await page.goto('/workgroup/madeup-workgroup-two')
+  await page.goto('./workgroup/madeup-workgroup-two')
   await page.waitForSelector('h1')
   await expect(page.locator('h1')).toHaveText('workgroup:madeup-workgroup-two 🔗')
   await expect(page.locator('h2')).toHaveText('mockdata (m1)')
@@ -25,7 +32,7 @@ test('check that we can navigate to a detail page', async ({ page }) => {
 })
 
 test('check that we can toggle dark mode', async ({ page }) => {
-  await page.goto('/')
+  await page.goto('./')
   await page.locator('#theme-toggle').click()
   await expect(page.locator('html')).toHaveClass('dark')
   await page.locator('#theme-toggle').click()
@@ -34,7 +41,7 @@ test('check that we can toggle dark mode', async ({ page }) => {
 
 test('check that we can link to plain text searches', async ({ page }) => {
   const term = 'two'
-  await page.goto(`?searchstring=${encodeURIComponent(term)}`)
+  await page.goto(`./?searchstring=${encodeURIComponent(term)}`)
   await page.waitForSelector('input#search')
   await expect(page.locator('input#search')).toHaveValue(term)
   await expect(page.locator('table tbody tr')).toHaveCount(1)
@@ -43,7 +50,7 @@ test('check that we can link to plain text searches', async ({ page }) => {
 test('check that we can link to regex searches', async ({ page }) => {
   const term = 'two$'
   const isRegex = true
-  await page.goto(`?isRegex=${isRegex}&searchstring=${encodeURIComponent(term)}`)
+  await page.goto(`./?isRegex=${isRegex}&searchstring=${encodeURIComponent(term)}`)
   await page.waitForSelector('input#search')
 
   await expect(page.locator('input#search')).toHaveValue(term)
@@ -52,11 +59,11 @@ test('check that we can link to regex searches', async ({ page }) => {
 })
 
 test('check that 404 errors route and render correctly', async ({ page }) => {
-  await page.goto('/workgroup/nonexistent')
+  await page.goto('./workgroup/nonexistent')
   await expect(page.locator('h1')).toHaveText('404 Workgroup Not Found')
 })
 
 test('check that application errors render with provided details correctly', async ({ page }) => {
-  await page.goto('/error?err=1&details=%7B%22data%22%3A%22foobar%22%7D')
+  await page.goto('./error?err=1&details=%7B%22data%22%3A%22foobar%22%7D')
   await expect(page.locator('.monospace')).toHaveText('foobar')
 })
