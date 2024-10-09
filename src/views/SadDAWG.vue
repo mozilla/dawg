f2d6615dd1c0f9098ae04226238bd678b539de66
Corban Cloud <ccloud@mozilla.com>
2024-10-09 09:23:07 -0700

    feat: important feature add (#45)

---
+++ src/views/SadDAWG.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="container">
+        <img src="@/assets/dawg-search.png" alt="Looking for hot dawgs in your area?">
+    </div>
+</template>
+<style scoped>
+.container {
+    text-align: center;
+}
+
+img {
+    display: inline-block;
+    margin-top: 2rem;
+}
+</style>
